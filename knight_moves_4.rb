e5e6e8e74441e0f7c93b6eadd13c89b87f6f5018
Chris Mitchell <bobtheawesomechicken@gmail.com>
2021-09-28 20:40:50 -0500

    Make the parsing better

--- knight_moves_4.rb
+++ knight_moves_4.rb
@@ -40,33 +40,55 @@ end
 def parse_region_data(gd, gm)
   puts "Parsing region data..."
   regions = Array.new(gm[:y]) {|e| e = Array.new(gm[:x], -1)}
+  r_labels = []
   for i in gm[:r_start]..gm[:r_end] do
     cells = gd[i].split(',')
     for j in 0..gm[:x]-1 do
-      regions[i-2][j] = cells[j].to_i
+      if not r_labels.include? cells[j]
+        r_labels.push(cells[j])
+      end
+      new_label = r_labels.find_index(cells[j])
+      regions[i-2][j] = new_label.to_i
     end
   end
+  num_regions = r_labels.length
+  puts "num_regions = " + num_regions.to_s
   print_board(regions)
   puts
-  return regions
+  region_data = {
+    regions: regions,         # 2D array representing regions
+    num_regions: num_regions  # number of different regions
+  }
 end
 
 # Parse moves data from input file based on parsed metadata
 def parse_moves_data(gd, gm)
   puts "Parsing moves data..."
+  max_val = 0
   moves = Array.new(gm[:y]) {|e| e = Array.new(gm[:x], -1)}
   for i in gm[:m_start]..gm[:m_end] do
     cells = gd[i].split(',')
     for j in 0..gm[:x]-1 do
       moves[i-1-gm[:r_end]][j] = cells[j].to_i
+      if cells[j].to_i > max_val
+        max_val = cells[j].to_i
+      end
     end
   end
+  puts "max_val = " + max_val.to_s
   print_board(moves)
   puts
-  return moves
+  moves_data = {
+    moves: moves,     # 2D array representing positions of known cell values
+    max_val: max_val  # highest known cell value provided in the input file
+  }
 end
 
 grid_data = read_input_file
 grid_metadata = parse_grid_metadata(grid_data)
-regions = parse_region_data(grid_data, grid_metadata)
-moves = parse_moves_data(grid_data, grid_metadata)
+region_data = parse_region_data(grid_data, grid_metadata)
+moves_data = parse_moves_data(grid_data, grid_metadata)
+
+# Okay, so now I finally have all the logic in place to actually start doing
+#   the part that I started this thing for in the first place.
+# So, how shall I go about this...
