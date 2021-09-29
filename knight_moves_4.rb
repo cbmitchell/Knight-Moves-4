38b603ef7cdd22e95d3e8bcfd0980d383c9c4f47
Chris Mitchell <bobtheawesomechicken@gmail.com>
2021-09-28 20:11:27 -0500

    Methodize everything

--- knight_moves_4.rb
+++ knight_moves_4.rb
@@ -8,40 +8,65 @@ def print_board (board)
 end
 
 # Open input txt file containing grid info
-gridfile_name = ARGV[0]
-puts gridfile_name
-grid_data = File.read(gridfile_name).split
-
-# CHRIS -- Screw it. I'm just going to assume the order of the stuff in the file.
-x = grid_data[0].to_i
-y = grid_data[1].to_i
-r_start = 2               # r_start - first line of the input file describing the regions
-r_end = r_start + y - 1   # r_end   - last line of the input file describing the regions
-m_start = r_end + 1
-m_end = m_start + y - 1
-puts "r_start = " + r_start.to_s
-puts "r_end = " + r_end.to_s
-puts "m_start = " + m_start.to_s
-puts "m_end = " + m_end.to_s
-
-puts "Reading region data..."
-regions = Array.new(y) {|e| e = Array.new(x, -1)}
-for i in r_start..r_end do
-  cells = grid_data[i].split(',')
-  for j in 0..x-1 do
-    regions[i-2][j] = cells[j].to_i
-  end
+def read_input_file
+  gridfile_name = ARGV[0]
+  puts gridfile_name
+  grid_data = File.read(gridfile_name).split
 end
-print_board(regions)
-puts
 
-puts "Reading moves data..."
-moves = Array.new(y) {|e| e = Array.new(x, -1)}
-for i in m_start..m_end do
-  cells = grid_data[i].split(',')
-  for j in 0..x-1 do
-    moves[i-1-r_end][j] = cells[j].to_i
-  end
+def parse_grid_metadata(grid_data)
+  # CHRIS -- Screw it. I'm just going to assume the order of the stuff in the file.
+  x = grid_data[0].to_i
+  y = grid_data[1].to_i
+  r_start = 2               # r_start - first line of the input file describing the regions
+  r_end = r_start + y - 1   # r_end   - last line of the input file describing the regions
+  m_start = r_end + 1
+  m_end = m_start + y - 1
+  puts "r_start = " + r_start.to_s
+  puts "r_end = " + r_end.to_s
+  puts "m_start = " + m_start.to_s
+  puts "m_end = " + m_end.to_s
+  grid_metadata = {
+    x: x,
+    y: y,
+    r_start: r_start,
+    r_end: r_end,
+    m_start: m_start,
+    m_end: m_end
+  }
 end
-print_board(moves)
-puts
+
+# Parse region data from input file based on parsed metadata
+def parse_region_data(gd, gm)
+  puts "Parsing region data..."
+  regions = Array.new(gm[:y]) {|e| e = Array.new(gm[:x], -1)}
+  for i in gm[:r_start]..gm[:r_end] do
+    cells = gd[i].split(',')
+    for j in 0..gm[:x]-1 do
+      regions[i-2][j] = cells[j].to_i
+    end
+  end
+  print_board(regions)
+  puts
+  return regions
+end
+
+# Parse moves data from input file based on parsed metadata
+def parse_moves_data(gd, gm)
+  puts "Parsing moves data..."
+  moves = Array.new(gm[:y]) {|e| e = Array.new(gm[:x], -1)}
+  for i in gm[:m_start]..gm[:m_end] do
+    cells = gd[i].split(',')
+    for j in 0..gm[:x]-1 do
+      moves[i-1-gm[:r_end]][j] = cells[j].to_i
+    end
+  end
+  print_board(moves)
+  puts
+  return moves
+end
+
+grid_data = read_input_file
+grid_metadata = parse_grid_metadata(grid_data)
+regions = parse_region_data(grid_data, grid_metadata)
+moves = parse_moves_data(grid_data, grid_metadata)
