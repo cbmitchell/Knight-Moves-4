efe41d0e19628bf788856a3a40ec466e5480422c
Chris Mitchell <bobtheawesomechicken@gmail.com>
2021-10-12 13:42:50 -0500

    It actually kinda works sometimes

--- knight_moves_4_v2.rb
+++ knight_moves_4_v2.rb
@@ -25,9 +25,10 @@ def print_state_data(s)
   puts "regions: "
   print_board(s[:regions])
   puts "r_num_cells: " + s[:r_num_cells].to_s
+  puts "r_free_cells: " + s[:r_free_cells].to_s
   puts "r_known_cells: " + s[:r_known_cells].to_s
   puts "target_sum: " + s[:target_sum].to_s
-  puts "sum_of_greater_unknown_ms: " + s[:sum_of_greater_unknown_ms].to_s
+  # puts "sum_of_greater_unknown_ms: " + s[:sum_of_greater_unknown_ms].to_s
   puts "prospective_move: " + s[:prospective_move].to_s
   puts "solved: " + s[:solved].to_s
 end
@@ -163,29 +164,6 @@ def determine_possible_num_moves(gm, rd, md)
   return poss_ms
 end
 
-# Return a possible partitioning of m sequential values among r regions such
-#   that the sum of all values in a region is equivalent to the sum of all
-#   values in any other region.
-# Should be noted that the method used below only works because we know we're
-#   dealing with a complete triangular number sequence up to m.
-def calc_root_partition(r, m)
-  total_per_r = ((m * (m + 1)) / 2) / r
-  root_partition = Array.new(m, -1)
-  region_totals = Array.new(r, 0)
-  i = m
-  while i > 0 do
-    for j in 0..r-1 do
-      if (region_totals[j] + i <= total_per_r)
-        region_totals[j] += i
-        root_partition[i-1] = j
-        break
-      end
-    end
-    i = (i - 1)
-  end
-  return root_partition
-end
-
 # Calculate triangular number
 def tri(m)
   tri_m = (m * (m + 1)) / 2
@@ -277,29 +255,100 @@ end
 # ———————————————————— # ———————————————————— # ————————————————————
 # ———————————————————— # ———————————————————— # ————————————————————
 
+
+# Doing everything from scratch just based on moves, regions, & num_moves.
+# Not the most efficient way, but a pretty easy to understand way, so it will
+#   serve as a proof of concept for the time being and then be refined later.
+#
+# moves ---> m
+# moves ---> known_moves
+# moves ---> unknown_moves
+# m + known_moves ---> next_highest_known_m
+# m + known_moves + regions ---> r_known_cells
+# m + unknown_moves ---> next_highest_unknown_m
+# m + unknown_moves + r_num_cells + r_known_cells ---> sum_of_greater_unknown_ms
+def derive_moves_metadata(s)
+  puts "Deriving move metadata..."
+  known_moves = Array.new(s[:num_moves]) {|e| e = {x: -1, y: -1}}
+  unknown_moves = [*1..s[:num_moves]]
+  for y in 0...s[:moves].length do
+    row = s[:moves][y]
+    for x in 0...row.length do
+      if s[:moves][y][x] > 0
+        known_moves[s[:moves][y][x]-1] = {x: x, y: y}
+        unknown_moves = unknown_moves - [s[:moves][y][x]]
+      end
+    end
+  end
+  m = -1
+  if not unknown_moves.empty?
+    m = unknown_moves[0]
+  end
+  next_highest_known_m = -1
+  for i in m...known_moves.length do
+    if known_moves[i][:x] != -1
+      next_highest_known_m = i
+      break
+    end
+  end
+  next_highest_unknown_m = -1
+  unknown_moves.each do |unknown_move|
+    if unknown_move > m
+      next_highest_unknown_m = unknown_move
+      break
+    end
+  end
+  # I'm really bothered by this part, but as stated up top, this is being done
+  #   for the sake of simplicity for now, and will be refactored later...
+  # r_known_cells = Array.new(s[:r_num_cells].length, [])
+  r_known_cells = Array.new(s[:r_num_cells].length) {|e| e = Array.new()}
+  # r_free_cells = Array.new(s[:r_num_cells].length, 0)
+  r_free_cells = s[:r_num_cells].clone
+  for i in 0...known_moves.length do
+    next if known_moves[i][:x] == -1
+    r = s[:regions][known_moves[i][:y]][known_moves[i][:x]]
+    r_free_cells[r] -= 1
+    r_known_cells[r].push(i+1)
+  end
+  s[:m] = m
+  s[:known_moves] = known_moves
+  s[:unknown_moves] = unknown_moves
+  s[:next_highest_known_m] = next_highest_known_m
+  s[:next_highest_unknown_m] = next_highest_unknown_m
+  s[:r_known_cells] = r_known_cells
+  s[:r_free_cells] = r_free_cells
+end
+
 # Find potential first move
 def first_moves(s)
-  # m = s[:unknown_moves][0]     # Find first unknown move
-  s[:m] = s[:unknown_moves][0]
+  s[:m] = s[:unknown_moves][0]    # Find first unknown move
   poss_moves = get_poss_next_moves(s)
   return poss_moves
 end
-
 
 def validate_state(s)
   puts "Validating state..."
   # Pretty sure the way I'm currently doing this, I'm not applying the prospective_move
   #   before validation the first time around, so this might not be good... !!!***
-  r_known_cells = s[:r_known_cells][s[:prospective_move][:r]]
-  r_num_cells = s[:r_num_cells][s[:prospective_move][:r]]
-  actual_sum = r_known_cells.inject(0, :+)
+  # print_state_data(s)
+  r = s[:prospective_move][:r]
+  actual_sum = s[:r_known_cells][r].inject(0, :+)
   difference = s[:target_sum] - actual_sum
+  sum_of_greater_unknown_ms = 0
+  i = s[:unknown_moves].length-1
+  while i >= 0 and i >= s[:unknown_moves].length - s[:r_free_cells][r]
+    sum_of_greater_unknown_ms = sum_of_greater_unknown_ms + s[:unknown_moves][i]
+    i -= 1
+  end
+  if difference == 0
+    return true
+  end
   if difference < s[:next_highest_unknown_m]
-    puts "INVALID STATE -- r " + s[:prospective_move][:r].to_s + " is overfilled (target_sum = " + s[:target_sum].to_s + ")"
+    puts "INVALID STATE -- r " + r.to_s + " is overfilled (target_sum = " + s[:target_sum].to_s + ")"
     return false # r is overfilled
   end
-  if difference > s[:sum_of_greater_unknown_ms]
-    puts "INVALID STATE -- r " + s[:prospective_move][:r].to_s + " is underfilled (target_sum = " + s[:target_sum].to_s + ")"
+  if difference > sum_of_greater_unknown_ms
+    puts "INVALID STATE -- r " + r.to_s + " is underfilled (target_sum = " + s[:target_sum].to_s + ")"
     return false # r is underfilled
   end
   return true
@@ -353,41 +402,17 @@ def derive_state_metadata(s)
   s[:sum_of_greater_unknown_ms] = sum_of_greater_unknown_ms
 end
 
-# Separate note from the above:
-# I may also consider splitting up my state metadata object into two different objects:
-#   * state_data
-#     * Contains the minimum set of data required to derive everything else.
-#   * state_metadata
-#     * Contains all the values derived from values contained in state_data.
-
 # Modifies relevant state variables based on the state's current :prospective_move.
 # Returns nothing as it is directly modifiying the state metadata object.
 def apply_move(s) #TODO -- DOUBLE-CHECK THE LOGIC FOR THIS FUNCTION!!!***
   puts "\nApplying move..."
-  derive_state_metadata(s)
-  print_state_data(s)
   s[:moves][s[:prospective_move][:y]][s[:prospective_move][:x]] = s[:m]
-  s[:r_known_cells][s[:prospective_move][:r]].push(s[:m])
-  s[:known_moves][s[:m]][:x] = s[:prospective_move][:x]
-  s[:known_moves][s[:m]][:y] = s[:prospective_move][:y]
-  s[:unknown_moves] = s[:unknown_moves] - [s[:m]]
-  s[:m] = s[:m] + 1 # Not ENTIRELY sure about this, but we'll see how it goes...
-  derive_state_metadata(s)
+  # print_state_data(s)
+  derive_moves_metadata(s)
+  # print_state_data(s)
 end
 
-# Modifies relevant state variables based on the state's current :prospective_move.
-# Returns nothing as it is directly modifiying the state metadata object.
-# def apply_move(s) #TODO -- DOUBLE-CHECK THE LOGIC FOR THIS FUNCTION!!!***
-#   puts "\nApplying move..."
-#   s[:moves][s[:prospective_move][:y]][s[:prospective_move][:x]] = s[:m]
-#   s[:m] = s[:m] + 1 # Not ENTIRELY sure about this, but we'll see how it goes...
-#   # print_state_data(s)
-#   derive_state_metadata(s)
-# end
-
 # Inverse of the apply_move(s) method defined above.
-# We'll need to make sure that this function doesn't undo any
-#   given moves. This will require a new state variable, given_moves.
 def undo_move(s) #TODO -- DOUBLE-CHECK THE LOGIC FOR THIS FUNCTION!!!***
   puts "\nUndoing move..."
   s[:m] = s[:m] - 1
@@ -395,19 +420,11 @@ def undo_move(s) #TODO -- DOUBLE-CHECK THE LOGIC FOR THIS FUNCTION!!!***
     s[:m] = s[:m] -1
   end
   xy = s[:known_moves][s[:m]-1]
-  s[:known_moves][s[:m]-1] = {x: -1, y: -1}
-  r = s[:regions][xy[:y]][xy[:x]]
   s[:moves][xy[:y]][xy[:x]] = 0
-  puts "     r = " + r.to_s
-  s[:r_known_cells][r].pop()
-  s[:unknown_moves] = s[:unknown_moves].push(s[:m]).sort.reverse()
-  derive_state_metadata(s)
+  # print_state_data(s)
+  derive_moves_metadata(s)
+  # print_state_data(s)
 end
-
-# Well, everything else is theoretically in place. Now I just need to
-#   program a solution to the regular version of the Knight's Tour.
-# How hard could that possibly be...?
-# Anyway...
 
 # Returns an array of {:x, :y} objects which represent open positions on the board
 #   that are one knight move away from the position of move m.
@@ -513,28 +530,8 @@ def recursive_function(s)
     puts "A solution has been found."
     return s
   end
-  derive_state_metadata(s) # CHRIS -- Not so sure about this...
-  # # TODO:  Have logic to initially insert a prospective move or have the
-  #          derive_state_metadata function not rely on it.
-  # print_state_data(s)
-  # ------ get_poss_next_moves ------
+  derive_moves_metadata(s) # CHRIS -- Not so sure about this...
   poss_moves = get_poss_next_moves(s)
-  # if s[:known_moves][s[:m]-1][:x] != -1     # If the current move is already known...
-  #   poss_moves = [s[:known_moves][s[:m]-1]] # ...only possible next move is that move.
-  # else
-  #   next_lowest_known_m = s[:m] - 1
-  #   poss_moves_from_prev = get_poss_moves_from_prev(s)
-  #   poss_moves_from_next = get_poss_moves_from_next(s)
-  #   if not poss_moves_from_next.empty?
-  #     if not poss_moves_from_prev.empty?
-  #       poss_moves = poss_moves_from_prev & poss_moves_from_next
-  #     else
-  #       poss_moves = poss_moves_from_next
-  #     end
-  #   else
-  #     poss_moves = poss_moves_from_prev
-  #   end
-  # end
   while not poss_moves.empty?
     puts "poss_moves: " + poss_moves.to_s
     s[:prospective_move] = poss_moves.shift()
@@ -547,189 +544,6 @@ def recursive_function(s)
     undo_move(s)
   end
   return s
-end
-
-# It should be noted that this function only calculates all possible partitions
-#   of the same length as the provided root_partition.
-#
-# Well, I just tried running this on a 5x5 example, and it took WAY too long.
-# As a matter of fact, I got impatient and stopped it before it finished.
-# Anyway, my point is that we need to figure out a way to decrease the time
-#   complexity on this beast or we're boned. As it currently is, the thing just
-#   takes FAR too long to run...
-# So it would stand to reason that I can decrease the complexity of this thing
-#   by introducing more insight I have about the nature of the problem and its solutions.
-#   This includes the following:
-#     -
-def calc_all_poss_partitions(root_partition, rd, md)
-  # puts "\nCalculating all possible partitions of length " + root_partition.length.to_s
-  root_p = normalize_partition(root_partition)
-  unvisited_ps = [root_p]
-  unique_ps = []
-  valid_ps = []
-  while not unvisited_ps.empty? do
-    p = unvisited_ps.shift
-    # puts "Visiting partition  " + p.to_s + "... "
-    if not unique_ps.include?(p)
-      unique_ps.push(p)
-      # puts p.to_s + " is unique - Now expanding... "
-      # NOTE: It might be a bit cleaner to have a function that gets all
-      #   neighboring partitions, and then have that be SEPARATE from
-      #   the function that validates them. Still, this would result in
-      #   an absolutely massive neighboring_ps for larger examples, and
-      #   that would be bad in terms of space and time...
-      neighboring_ps = calc_poss_partitions(p, rd, md)
-      neighboring_ps.each do |n|
-        unvisited_ps.push(n)
-      end
-      if validate_partition(p, rd, md)
-        valid_ps.push(p)
-      end
-    end
-  end
-  # puts "calc_all_poss_partitions resulted in " + valid_ps.length.to_s + " partitions."
-  return valid_ps
-end
-
-# Returns an array of arrays, where each array represents a unique, valid
-#   partition given r & m.
-# This array of partitions is not comprehensive. All the partitions in the array
-#   are only one step removed from the provided root_partition, where a "step"
-#   is defined as a swapping of summands between only two regions such that the
-#   regions retain their original sums.
-# Could definitely be optimized. Currently, it is quite inefficient...
-# I should also mention that this thing has really really terrible big O.
-#   I mean, if this gets up to even 25 possible moves, it produces a downright
-#   absurd number of possible partitions.
-# This function essentially EXPANDS A NODE where each node is a unique partition.
-# To the above point, this function is used to create a tree of nodes from inside
-#   the calc_all_poss_partitions(...) method above.
-def calc_poss_partitions(root_partition, rd, md)
-  root_partition = normalize_partition(root_partition)
-  # puts "root_partition    = " + root_partition.to_s
-  r = rd[:r_num_cells].length
-  poss_partitions = Array.new
-  # poss_partitions.push(root_partition)
-  xxs = create_r_arrays_from_partition(rd[:r_num_cells], root_partition)
-
-  i_j_combinations = (0...xxs.length).to_a.combination(2).to_a
-  i_j_combinations.each do |ij|
-    xx_i_combinations = (1...xxs[ij[0]].length).flat_map{|size| xxs[ij[0]].combination(size).to_a}
-    xx_j_combinations = (1...xxs[ij[1]].length).flat_map{|size| xxs[ij[1]].combination(size).to_a}
-    xx_i_combinations.each do |xx_i_combination|
-      xx_i_sum = xx_i_combination.inject(0, :+)
-      xx_j_combinations.each do |xx_j_combination|
-        xx_j_sum = xx_j_combination.inject(0, :+)
-        # This part is horribly inefficient. I already know some ways
-        #   it could be improved, but I just want a proof of concept for
-        #   the time being, so maybe I'll come back and optimize later.
-        if xx_i_sum == xx_j_sum
-          new_partition = create_new_partition(root_partition, xx_i_combination, xx_j_combination)
-          new_partition = normalize_partition(new_partition)
-          # Originally, I had this part cut down the number of partitions it returned by
-          #   making it only return VALID partitions. Now that I'm essentially using this
-          #   as a method of EXPANDING from a given partition, I should allow it to return
-          #   invalid partitions, as they could still have valid children which would not
-          #   be reachable otherwise.
-          # if (not poss_partitions.include? new_partition) and validate_partition(new_partition, rd, md)
-          if not poss_partitions.include? new_partition
-            poss_partitions.push(new_partition)
-            # puts "poss_partition[" + (poss_partitions.length-1).to_s + "] = " + new_partition.to_s
-          end
-        end
-      end
-    end
-  end
-  return poss_partitions
-end
-
-# - Create r arrays,
-#   Each array will represent one region of the root_partition.
-#   These arrays will contain integers representing the values in that region that
-#   contribute to the region's sum.
-def create_r_arrays_from_partition(r_num_cells, root_partition)
-  xxs = Array.new(r_num_cells.length) {|e| e = Array.new}
-  for i in 1..root_partition.length
-    xxs[root_partition[i-1]].push(i)
-  end
-  return xxs
-end
-
-def create_new_partition(base_partition, xx_i, xx_j)
-  new_partition = base_partition.clone
-  i = base_partition[xx_i[0]-1]
-  j = base_partition[xx_j[0]-1]
-  xx_i.each do |x_i|
-    new_partition[x_i-1] = j
-  end
-  xx_j.each do |x_j|
-    new_partition[x_j-1] = i
-  end
-  return new_partition
-end
-
-def normalize_partition(partition)
-  r_labels = []
-  for i in 0...partition.length
-    if not r_labels.include? partition[i]
-      r_labels.push(partition[i])
-    end
-    new_label = r_labels.find_index(partition[i])
-    partition[i] = new_label
-  end
-  return partition
-end
-
-# Validate partition based on the following:
-#   1. r_num_cells
-#   2. moves (known cell values and their associated regions)
-def validate_partition(partition, rd, md)
-
-  # Validate based on region data
-  r_num_cells_sorted = rd[:r_num_cells].sort
-  p_num_cells = Array.new(rd[:r_num_cells].length, 0)
-  for i in 0...partition.length do
-    p_num_cells[partition[i]] += 1
-  end
-  p_num_cells_sorted = p_num_cells.sort
-  for i in 0...p_num_cells_sorted.length do
-    if r_num_cells_sorted[i] < p_num_cells_sorted[i]
-      return false
-    end
-  end
-
-  # Validate based on moves data
-  xxs = create_r_arrays_from_partition(rd[:r_num_cells], partition)
-  ms_per_r = rd[:ms_per_r]
-  for r in 0...ms_per_r.length do
-    next if ms_per_r[r].empty?
-    max_size = rd[:r_num_cells][r]
-    for i in 0...ms_per_r[r].length do
-      partition_r_containing_m = partition[ms_per_r[r][i]-1]
-      actual_size = xxs[partition_r_containing_m].length
-      if actual_size > max_size
-        return false
-      end
-    end
-  end
-  return true
-end
-
-# Validates whether or not a proposed partition conforms to the r_num_cells
-# Returns true if valid, false otherwise.
-def validate_partition_regions(partition, r_num_cells)
-  r_num_cells_sorted = r_num_cells.sort
-  p_num_cells = Array.new(r_num_cells.length, 0)
-  for i in 0...partition.length do
-    p_num_cells[partition[i]] += 1
-  end
-  p_num_cells_sorted = p_num_cells.sort
-  for i in 0...p_num_cells_sorted.length do
-    if r_num_cells_sorted[i] < p_num_cells_sorted[i]
-      return false
-    end
-  end
-  return true
 end
 
 def initialize_state_data(rd, md)
@@ -765,23 +579,42 @@ state_data = initialize_state_data(region_data, moves_data)
 print_state_data(state_data)
 poss_moves = first_moves(state_data)
 puts "\nposs_moves: " + poss_moves.to_s
+
+# CHRIS -- HARD-CODING THE BELOW FOR TESTING PURPOSES !!!***
+# It's meant to go with grid_input_test_12.txt
+# state_data[:num_moves] = 9
+# poss_moves.each do |poss_move|
+#   state_data[:prospective_move][:x] = poss_move[:x]
+#   state_data[:prospective_move][:y] = poss_move[:y]
+#   state_data[:prospective_move][:r] = state_data[:regions][state_data[:prospective_move][:y]][state_data[:prospective_move][:x]]
+#   state_data[:target_sum] = tri(state_data[:num_moves]) / state_data[:r_num_cells].length
+#   derive_moves_metadata(state_data)
+#   recursive_function(state_data)
+# end
+
+solutions = []
 moves_data[:poss_ms].each do |poss_max_m|
   state_data[:num_moves] = poss_max_m
   puts "\n\nNew poss_max_m = " + poss_max_m.to_s
   poss_moves.each do |poss_move|
     state_data[:prospective_move][:x] = poss_move[:x]
     state_data[:prospective_move][:y] = poss_move[:y]
+    state_data[:prospective_move][:r] = state_data[:regions][state_data[:prospective_move][:y]][state_data[:prospective_move][:x]]
+    state_data[:target_sum] = tri(state_data[:num_moves]) / state_data[:r_num_cells].length
     # print_state_data(state_data)
     # puts "\nderiving state metadata..."
-    derive_state_metadata(state_data)
+    derive_moves_metadata(state_data)
     # print_state_data(state_data)
     recursive_function(state_data)
+    if state_data[:solved]
+      # CHRIS -- This will probably require a deep copy, actually...
+      solutions.push(state_data[:moves])
+    end
   end
 end
-
-
-# puts "Printing initial state data:"
-# print_state_data(state_data)
-# derive_state_metadata(state_data)
-# puts "Printing newly-derived state data:"
-# print_state_data(state_data)
+i = 1
+solutions.each do |solution|
+  puts "\nPossible solution " + i.to_s + ":"
+  print_board(solution)
+  i += 1
+end
